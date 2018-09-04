0e0c0243f17818b1d06918e99d712b60fdf8cec2
Wojaczek27 <wojaczek27@gmail.com>
2018-09-04 13:00:03 +0200

    the original files, need some external jars

---
+++ TDDC17Lab1/src/TDDC17Lab1/MyVacuumAgent.java
@@ -0,0 +1,9 @@
+package src.TDDC17Lab1;
+
+import aima.core.agent.impl.AbstractAgent;
+
+public class MyVacuumAgent extends AbstractAgent {
+    public MyVacuumAgent() {
+    	super(new MyAgentProgram());
+	}
+}
