fe4f4b737ef73e80c2c78e773bb9194e4c6a6c94
danwa223 <danwa223@student.liu.se>
2018-09-18 13:14:24 +0200

    Added a lot of general graph search stuff. See comments.

--- lab2/src/searchCustom/CustomGraphSearch.java
+++ lab2/src/searchCustom/CustomGraphSearch.java
@@ -16,6 +16,8 @@ public class CustomGraphSearch implements SearchObject {
 	private NodeQueue frontier;
 	protected ArrayList<SearchNode> path;
 	private boolean insertFront;
+	
+	private SearchNode currentNode;
 
 	/**
 	 * The constructor tells graph search whether it should insert nodes to front or back of the frontier 
@@ -29,20 +31,73 @@ public class CustomGraphSearch implements SearchObject {
 	 */
 	public ArrayList<SearchNode> search(Problem p) {
 		// The frontier is a queue of expanded SearchNodes not processed yet
-		frontier = new NodeQueue();
+		frontier = new NodeQueue(); //FIFO
 		/// The explored set is a set of nodes that have been processed 
 		explored = new HashSet<SearchNode>();
 		// The start state is given
 		GridPos startState = (GridPos) p.getInitialState();
 		// Initialize the frontier with the start state  
 		frontier.addNodeToFront(new SearchNode(startState));
-
 		// Path will be empty until we find the goal.
 		path = new ArrayList<SearchNode>();
 		
-		// Implement this!
-		System.out.println("Implement CustomGraphSearch.java!");
+		// OWN ADDITION: GET GOALSTATE
+		GridPos goalState = (GridPos) p.getGoalState();
+				
+		// Frontier is empty besides the start node.
 		
+		while (true){ //do loop
+			
+			//if the frontier is empty return failure
+			if (frontier.isEmpty()){
+				return path; //return failure?
+			}
+			
+			//choose a leaf node and remove it from the frontier -- HERE IS WHERE BFS OR DFS COMES IN
+			// --- currently is a FIFO implementation that doesn't care about logic.
+			currentNode = frontier.peekAtFront();
+			frontier.removeFirst();
+			path.add(currentNode);
+			
+			//if the node contains a goal state then return the corresponding solution
+			if (currentNode.getState().equals(goalState)){
+				//we found the goal
+				return path;
+			}
+			
+			//add the node to the explored set
+			explored.add(currentNode);
+			
+			//expand the chosen node, adding the resulting nodes to the frontier - ONLY IF NOT IN THE FRONTIER OR IN EXPLORED SET
+			GridPos currentPosition = currentNode.getState();
+			int currentX = currentPosition.getX();
+			int currentY = currentPosition.getY();
+			//check the neighbouring nodes if they are a) not explored, b) not already added (and possibly c) free?)
+			for (int xPos = currentX-1; xPos < currentX+2; currentX++){
+				for (int yPos = currentY-1; yPos < currentY+2; currentY++){
+					if (xPos == currentX  && yPos == currentY){
+						break; //we started from this position and we don't want to work with it again.
+					} else {
+						SearchNode neighbourNode = new SearchNode(new GridPos(xPos, yPos));
+						//Explored is a hashSet so it should work without problems. Frontier on the other hand is a custom-built class using a Linked List. Will it use equals() or not?
+						if (!(explored.contains(neighbourNode) || (frontier.contains(neighbourNode)))){ //Do we need to care about if the neighbouring node is a wall?
+							frontier.addNodeToBack(neighbourNode);
+						}
+					}
+				}
+			}
+		}
+		
+		// Implement this!
+		//System.out.println("Implement CustomGraphSearch.java!");
+		
+		//This thing below is wrong.
+		/*for (int i = 0; i < 50; i++){
+			for (int j = 0; j < 50; j++){
+				frontier.addNodeToBack(new SearchNode(new GridPos(i, j), frontier.peekAtBack())); //This adds all nodes to the "unexplored" list. But ignores all logic.
+				//It also adds the start node again.
+			}
+		}*/
 		
 		/* Some hints:
 		 * -Read early part of chapter 3 in the book!
@@ -68,7 +123,7 @@ public class CustomGraphSearch implements SearchObject {
 		 *  When the goal is found, the path to be returned can be found by: path = node.getPathFromRoot();
 		 */
 		/* Note: Returning an empty path signals that no path exists */
-		return path;
+		//return path;
 	}
 
 	/*
