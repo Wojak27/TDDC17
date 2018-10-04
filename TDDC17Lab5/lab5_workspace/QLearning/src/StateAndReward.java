b7d975c454b43ff818496f242be25a0e0e22d616
Karol Wojtulewicz <karwo001@student.liu.se>
2018-10-04 10:06:34 +0200

    36 states

--- TDDC17Lab5/lab5_workspace/QLearning/src/StateAndReward.java
+++ TDDC17Lab5/lab5_workspace/QLearning/src/StateAndReward.java
@@ -16,10 +16,13 @@ public class StateAndReward {
 		/* TODO: IMPLEMENT THIS FUNCTION */
 		String state;
 		angle = round(angle);					// karol
+		if(Math.abs(angle) > 1){
+			state = "Wrong angle!";
+		}else
 		if(Math.abs(angle) < 0.2 && vy<1 && vy>-5){
 			if(Math.abs(vy) < 0.1) state = "Perfect!";
 			else if(vy >-2) state = "almost there";
-			state = "Just right angle!";
+			else state = "Just right angle!";
 		}else if(angle<0){
 			state = "Tipping left!";
 		}else{
@@ -34,10 +37,12 @@ public class StateAndReward {
 
 		/* TODO: IMPLEMENT THIS FUNCTION */
 		angle = round(angle);
-		if(Math.abs(angle) < 0.2 && vy<1 && vy>-5){
+		if(Math.abs(angle) > 1){
+			return -100;
+		}else if(Math.abs(angle) < 0.2 && vy<1 && vy>-5){
 			if(Math.abs(vy) < 0.1) return 100;
-			else if(vy >-2) return (10 + vy);
-			return 1;
+			else if(vy >-2) return (10 - vy);
+			return 2;
 		}
 		double reward = angle < 0 ? angle : -angle; //karol
 
@@ -46,22 +51,48 @@ public class StateAndReward {
 
 	/* State discretization function for the full hover controller */
 	public static String getStateHover(double angle, double vx, double vy) {
-
-		/* TODO: IMPLEMENT THIS FUNCTION */
-
-		String state = "OneStateToRuleThemAll2";
+		angle = round(angle);
 		
-		return state;
+		if(Math.abs(angle) < 0.1)  return "Nice angle! "+getStateVel(angle, vx, vy);
+		else if (Math.abs(angle) < 0.3)  return "Very good angle! " +getStateVel(angle, vx, vy);
+		else if (Math.abs(angle) < 0.5)  return "good angle! "+getStateVel(angle, vx, vy);
+		else if (Math.abs(angle) < 0.7)  return "Okay angle! " +getStateVel(angle, vx, vy);
+		else if (Math.abs(angle) < 1)  return "decent angle! " +getStateVel(angle, vx, vy);
+		else return "Totaly wrong angle! "+getStateVel(angle, vx, vy);
+		
+	}
+	
+	public static String getStateVel(double angle, double vx, double vy) {
+		angle = round(angle);
+		
+		if(Math.abs(vy) < 0.1)  return "Nice vel!";
+		else if (Math.abs(vy) < 0.5)  return "Very good vel!";
+		else if (Math.abs(vy) < 1)  return "good wrong vel!";
+		else if (Math.abs(vy) < 2)  return "Okay wrong vel!";
+		else if (Math.abs(vy) < 3)  return "decent wrong vel!";
+		else return "Totaly wrong vel!";
+		
 	}
 
 	/* Reward function for the full hover controller */
 	public static double getRewardHover(double angle, double vx, double vy) {
-
-		/* TODO: IMPLEMENT THIS FUNCTION */
+		double rewardVel = 0;
+		double rewardAng = 0;
+		double alpha = 4;
+		double beta = 1;
+		angle = round(angle);
+		if (Math.abs(angle) < 1){
+			rewardAng = (Math.pow((Math.PI-Math.abs(angle)),2));
+		}else rewardAng = -100;
 		
-		double reward = 0;
-
-		return reward;
+		rewardVel = estimateVelocityReweward(vx, vy);
+		
+		return rewardVel*alpha + rewardAng*beta;
+	}
+	
+	private static double estimateVelocityReweward(double vx, double vy){
+		// Three states
+		return (3-Math.abs(vy));
 	}
 
 	// ///////////////////////////////////////////////////////////
